7b1a6b52f6b644c07c9c148afa8c8717895923df
Alexander Juarez <alexander.73012317@gmail.com>
2024-09-27 16:27:40 -0700

    Commit

--- Program.cs
+++ Program.cs
@@ -11,11 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 
 app.MapControllers();
